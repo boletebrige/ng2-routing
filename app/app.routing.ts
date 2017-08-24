ba80403f0761e385de456f1d572c02e628f7b732
boletebrige <josip.ravas.broj@gmail.com>
2017-08-24 15:25:20 +0200

    router links

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,3 +1,4 @@
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -9,4 +10,4 @@ const appRoutes: Routes = [
     { path: 'contact', component: ContactComponent }
 ];
 
-export const appRouting = RouterModule.forRoot(appRoutes);
+export const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
