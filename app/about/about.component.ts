f5cb0ffa99445499d982775edbbed09acc8accb2
boletebrige <josip.ravas.broj@gmail.com>
2017-08-25 12:41:57 +0200

    nothing special users page edited and type safety added

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -1,4 +1,32 @@
 import { Component } from '@angular/core';
+// this is beacause of type safety it isn't necessary
+export class User {
+    id: number;
+    name: string;
+    username: string;
+    avatar: string;
+}
+
+const users: User[] = [
+    {
+        id: 1,
+        name: 'Josip',
+        username: 'boletebrige',
+        avatar: 'https://pbs.twimg.com/profile_images/827146559192170496/hxBWvDiC_400x400.jpg'
+    },
+    {
+        id: 2,
+        name: 'Nema',
+        username: 'nikad',
+        avatar: 'https://pbs.twimg.com/profile_images/738655910067851264/kMTd4Z8c_400x400.jpg'
+    },
+    {
+        id: 3,
+        name: 'Niko',
+        username: 'nije',
+        avatar: 'https://pbs.twimg.com/profile_images/693910281945243648/Fov72uA0_400x400.jpg'
+    }
+]
 
 @Component({
     selector: 'about-page',
@@ -6,4 +34,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./app/about/about.component.css']
 })
 
-export class AboutComponent {}
+export class AboutComponent {
+    users: User[] = users;
+}
--- dist/about/about.component.js
+++ dist/about/about.component.js
@@ -7,8 +7,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+// this is beacause of type safety it isn't necessary
+var User = (function () {
+    function User() {
+    }
+    return User;
+}());
+exports.User = User;
+var users = [
+    {
+        id: 1,
+        name: 'Josip',
+        username: 'boletebrige',
+        avatar: 'https://pbs.twimg.com/profile_images/827146559192170496/hxBWvDiC_400x400.jpg'
+    },
+    {
+        id: 2,
+        name: 'Nema',
+        username: 'nikad',
+        avatar: 'https://pbs.twimg.com/profile_images/738655910067851264/kMTd4Z8c_400x400.jpg'
+    },
+    {
+        id: 3,
+        name: 'Niko',
+        username: 'nije',
+        avatar: 'https://pbs.twimg.com/profile_images/693910281945243648/Fov72uA0_400x400.jpg'
+    }
+];
 var AboutComponent = (function () {
     function AboutComponent() {
+        this.users = users;
     }
     return AboutComponent;
 }());
